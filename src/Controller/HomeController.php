6dc75a868226e794162ad9b790251f70ec91fa01
fredstp <freal1802@gmail.com>
2021-08-18 16:28:14 +0200

    Doctrine + fixtures(faker,faker price)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -10,8 +11,10 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="homepage")
      */
-    public function homepage()
+    public function homepage(ProductRepository $productRepository)
     {
+        $product =  $productRepository->find(3);
+        dump($product);
         return $this->render('home.html.twig');
     }
 }
