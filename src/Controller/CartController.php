036b28a71a66c37a48b92e2506abd874a8387c26
fredstp <freal1802@gmail.com>
2021-09-14 16:51:45 +0200

    Gestion du panier en session

---
+++ src/Controller/CartController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Controller;
+
+use App\Cart\CartService;
+use App\Repository\ProductRepository;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+class CartController extends AbstractController
+{
+    /**
+     *@var ProductRepository
+     */
+    protected $productRepository;
+
+    /**
+     * @var CartService
+     */
+    protected $cartService;
+
+    public function __construct(ProductRepository $productRepository, CartService $cartService)
+    {
+
+        $this->productRepository = $productRepository;
+        $this->cartService = $cartService;
+    }
+
+    /**
+     * @Route("/cart/add/{id}", name="cart_add")
+     */
+    public function add($id, Request $request)
+    {
+        //0. Sécurisation : est-ce que le produit existe? 
+        $product = $this->productRepository->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException("le produit $id n'existe pas!");
+        }
+
+        $this->cartService->add($id);
+
+        $this->addFlash('success', "Le produit à bien été ajouté au panier");
+
+        if ($request->query->get('returnToCart')) {
+
+            return $this->redirectToRoute("cart_show");
+        }
+
+
+        return $this->redirectToRoute('product_show', [
+            'category_slug' => $product->getCategory()->getSlug(),
+            'slug' => $product->getSlug()
+        ]);
+    }
+
+    /**
+     * @Route("/cart", name="cart_show")
+     */
+    public function show()
+    {
+        $detailedCart = $this->cartService->getDetailedCartItems();
+
+        $total = $this->cartService->getTotal();
+
+
+        return $this->render('cart/index.html.twig', [
+            'items' => $detailedCart,
+            'total' => $total
+        ]);
+    }
+
+    /**
+     * @Route("/cart/delete/{id}", name="cart_delete", requirements={"id": "\d+"})
+     */
+    public function delete($id)
+    {
+        $product = $this->productRepository->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException("Le produit $id n'existe pas et ne peux pas être supprimé !");
+        }
+
+        $this->cartService->remove($id);
+
+        $this->addFlash("success", "Le produit à bien été supprimé du panier");
+
+        return $this->redirectToRoute("cart_show");
+    }
+
+    /**
+     * @Route("/cart/decrement/{id}", name="cart_decrement", requirements={"id": "\d+"})
+     */
+    public function decrement($id)
+    {
+        $product = $this->productRepository->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException("Le produit $id n'existe pas et ne peux pas être décrémenté !");
+        }
+
+        $this->cartService->decrement($id);
+
+        $this->addFlash("success", "le produit a bien été décrémenté");
+
+        return $this->redirectToRoute("cart_show");
+    }
+}
